b7db5ae029d3809925466de82456c914951160d6
Valencius Rianto <valencius.rianto25@gmail.com>
2025-02-21 16:59:43 +0700

    feat() : added tailwind, navbar and other

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { AppBar, Button } from "@mui/material";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ResponsiveAppBar />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button variant="contained">TEST</Button>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
