1413ac7ddd4c7e000d97bdf20efb69b5e9f420e3
Valencius Rianto <valencius.rianto25@gmail.com>
2025-02-26 15:29:36 +0700

    feat(navbar) : navbar done + template + routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
-import { AppBar, Button } from "@mui/material";
-import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import Home from "./pages/home/Home";
+import Portofolio from "./pages/portofolio/Portofolio";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ResponsiveAppBar />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="contained">TEST</Button>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portofolio" element={<Portofolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
