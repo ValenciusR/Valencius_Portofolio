cbea0b3972a62fb5ee705fb0283825131f3916d6
Valencius Rianto <valencius.rianto25@gmail.com>
2025-02-26 16:36:34 +0700

    feat(homePage) : homepage done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import Profile from "../../assets/home.jpg";
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./home.css";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className="grid home section">
+      <img src={Profile} alt="" className="home__img" />
 
-export default Home
+      <div className="home__content">
+        <div className="home__data">
+          <h1 className="home__title">
+            <span>I'm Valencius Rianto.</span> Developer
+          </h1>
+          <p className="home__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+            similique a dignissimos, animi voluptatibus error cupiditate
+            corporis aliquid, ab in eaque recusandae, nemo sint. Quaerat
+            eligendi ullam fugit voluptatem sint!
+          </p>
+
+          <Link to="/about" className="button">
+            More About Me{" "}
+            <span className="button__icon">
+              <FaArrowRight />
+            </span>
+          </Link>
+        </div>
+      </div>
+
+      <div className="color__block"></div>
+    </section>
+  );
+};
+
+export default Home;
