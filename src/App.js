01504108618dea20b11cf9c3eab212895870d1af
Valencius Rianto <valencius.rianto25@gmail.com>
2025-03-03 16:00:24 +0700

    feat(anim) : added anim changin page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
 import "./App.css";
 import Home from "./pages/home/Home";
@@ -8,18 +14,36 @@ import Contact from "./pages/contact/Contact";
 import Navbar from "./components/Navbar";
 import Themes from "./components/Themes";
 
+function AnimatedRoutes() {
+  const location = useLocation(); // ✅ useLocation inside Router
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: -100, opacity: 0 }}
+        transition={{ duration: 0.4 }}
+      >
+        <Routes location={location} key={location.pathname}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portofolio" element={<Portofolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Navbar />
       <Themes />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/portofolio" element={<Portofolio />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+      <AnimatedRoutes />
+    </Router>
   );
 }
 
